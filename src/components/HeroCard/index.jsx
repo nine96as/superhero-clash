ecfee3e45f0bee7fabad81564de9d61bcf29c9ba
nine96as <ashrafsaid79@gmail.com>
2023-12-14 16:14:48 +0000

    Pass hero payload props into elems

--- src/components/HeroCard/index.jsx
+++ src/components/HeroCard/index.jsx
@@ -1,10 +1,43 @@
 import React from 'react';
 
-const HeroCard = () => {
+import './style.css';
+const HeroCard = (props) => {
   return (
-    <>
-      <h2>HERO CARD</h2>
-    </>
+    <div className='hero-card'>
+      <h3 className='hero-name'> {props.name} </h3>
+      <img className='hero-img' src={props.image} alt={props.name} />
+
+      <ul>
+        <li className='attribute'>
+          Intelligence:
+          <button
+            onClick={() => props.handleStatCheck(props.stats.intelligence)}
+          >
+            {props.stats.intelligence == 'null'
+              ? 'N/A'
+              : props.stats.intelligence}
+          </button>
+        </li>
+        <li className='attribute'>
+          Strength:
+          <button onClick={() => props.handleStatCheck(props.stats.strength)}>
+            {props.stats.strength == 'null' ? 'N/A' : props.stats.strength}
+          </button>
+        </li>
+        <li className='attribute'>
+          Speed:
+          <button onClick={() => props.handleStatCheck(props.stats.speed)}>
+            {props.stats.speed == 'null' ? 'N/A' : props.stats.speed}
+          </button>
+        </li>
+        <li className='attribute'>
+          Durability:
+          <button onClick={() => props.handleStatCheck(props.stats.durability)}>
+            {props.stats.durability == 'null' ? 'N/A' : props.stats.durability}
+          </button>
+        </li>
+      </ul>
+    </div>
   );
 };
 

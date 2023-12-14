461f24542027b280d114e9422fa39e7a7ee1f40f
nine96as <nine96@skiff.com>
2023-12-14 09:56:13 +0000

    Add HomePage cmpt

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { HeroCard, ShuffleButton, VillainCard } from '../../components';
+
+const Home = () => {
+  return (
+    <>
+      <h1>Home</h1>
+      <HeroCard />
+      <VillainCard />
+      <ShuffleButton />
+    </>
+  );
+};
+
+export default Home;
